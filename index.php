0d994f43f1ece49b4a7a0709ba3eda40a8b050b7
rohitj1107 <rohit.jadhavk1107@gmail.com>
2020-10-28 13:33:02 +0530

    array_chunk

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  $arr = array('red','blue','yello','green','orange');
+  $ch = array_chunk($arr,2);
+  echo '<pre>';
+  print_r($ch);
+  echo '</pre>';
+?>
---
+++ readme.txt
@@ -0,0 +1,4 @@
+array_chunk()
+
+this function pass two argument one array value second is number to create
+array like separate array and make new array
